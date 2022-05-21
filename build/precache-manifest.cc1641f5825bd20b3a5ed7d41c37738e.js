905c3c194dd70ba4223ce41ce32a4c44215a43b8
Mohamed Awde <75025566+kemoday@users.noreply.github.com>
2022-05-21 07:17:50 +0200

    the build folder

---
+++ build/precache-manifest.cc1641f5825bd20b3a5ed7d41c37738e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4c0db3f9ffa9bc765a0781fe1c72671",
+    "url": "/polls-react-app/index.html"
+  },
+  {
+    "revision": "630bd4510d28ba752a42",
+    "url": "/polls-react-app/static/css/main.3d541e30.chunk.css"
+  },
+  {
+    "revision": "23cbf07b96c73c3b07ed",
+    "url": "/polls-react-app/static/js/2.4b04caab.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/polls-react-app/static/js/2.4b04caab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "630bd4510d28ba752a42",
+    "url": "/polls-react-app/static/js/main.a9540ff0.chunk.js"
+  },
+  {
+    "revision": "5e91e0916e5b68003f83",
+    "url": "/polls-react-app/static/js/runtime-main.bce9fcdc.js"
+  },
+  {
+    "revision": "c149d0efeef3cb2360e761c797b8e5e4",
+    "url": "/polls-react-app/static/media/statistics-illustration.c149d0ef.png"
+  },
+  {
+    "revision": "2a9c6f4cf00cf509d6dea026790acc4a",
+    "url": "/polls-react-app/static/media/vote.2a9c6f4c.jpg"
+  }
+]);
